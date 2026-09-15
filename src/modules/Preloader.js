4f1a6d1b76272b0dffb911bec8bc2d7513dd0ba4
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-15 07:01:01 +0700

    #96 Anime-Vibe: -Сохранение preloader в session storage, чтобы он отображался только 1 раз на главной странице за всю сессию

--- src/modules/Preloader.js
+++ src/modules/Preloader.js
@@ -1,19 +1,44 @@
 class Preloader {
   selectors = {
-    root: '[data-js-preloader]'
+    root: '[data-js-preloader]',
   }
 
   stateClasses = {
-    isLock: 'is-lock'
+    isLock: 'is-lock',
   }
+
+  sessionStorageKey = 'isPreloaderShow'
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root)
 
-    if (!this.rootElement) return;
+    if (!this.rootElement) {
+      return
+    }
+
+    if (this.isPreloaderShown()) {
+      this.removePreloader()
+      return
+    }
+
+    this.showPreloader()
+  }
+
+  isPreloaderShown = () => {
+    return sessionStorage.getItem(this.sessionStorageKey)
+  }
+
+  showPreloader = () => {
+    sessionStorage.setItem(this.sessionStorageKey, 'true')
 
     document.documentElement.classList.add(this.stateClasses.isLock)
+
     this.bindEvents()
+  }
+
+  removePreloader = () => {
+    this.rootElement.remove()
+    document.documentElement.classList.remove(this.stateClasses.isLock)
   }
 
   onAnimationEnd = () => {
@@ -21,7 +46,11 @@ class Preloader {
   }
 
   bindEvents() {
-    this.rootElement.addEventListener('animationend', this.onAnimationEnd )
+    this.rootElement.addEventListener(
+      'animationend',
+      this.onAnimationEnd,
+      { once: true },
+    )
   }
 }
 
