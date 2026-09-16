d9eb5a4c2ed92f80764936ea8fb9b3594e082615
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-16 13:15:21 +0700

    #105 Anime-Vibe: Видео

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,7 +1,7 @@
 import MovieBanner from '@/sections/MovieBanner'
 import MovieDetails from '@/sections/MovieDetails'
 import Seasons from '@/components/Seasons'
-import videoSrc from '@/assets/videos/chainsawman.mp4'
+import videoSrc from '@/assets/videos/placeholder.mp4'
 import posterSrc from '@/assets/images/posters/1.jpg'
 
 export const metadata = {
