9ad769db1ed564ea72b1d8f442164b5b4493c6e0
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-17 06:33:02 +0700

    #114 Anime-Vibe: Рефакторинг.

--- src/sections/Collections/Collections.jsx
+++ src/sections/Collections/Collections.jsx
@@ -13,6 +13,7 @@ const Collections = () => {
     <Tabs
       className="collections container"
       title="collections"
+      isEnableOnlyOnMobile
       items={collectionItems.map((collectionItems) => ({
         isActive: collectionItems.isActive,
         title: collectionItems.title,
